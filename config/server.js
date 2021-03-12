2166994087aa8ca2cd8f755329d2c159ab069720
Julio <juliioalbert98@gmail.com>
2021-03-11 18:57:43 -0600

    Backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1dfff6caad4bab95d333afcac66eb60'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '80a7ac54-3a34-4c32-a560-5beb23bfd504'
+};
